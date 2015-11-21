07d26b9fcec8a493fbef635961752a1c04dca23b
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-11-21 05:52:46 +0300

    rearrance

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,14 +1,11 @@
 use std::net::SocketAddr;
 
-
+use mio::{self, Token};
+use mio::tcp::TcpStream;
 use protobuf;
 
 mod server;
-mod utils;
-mod chunker;
-
-pub use self::server::Sender;
-pub use self::server::User;
+mod worker;
 
 pub fn start_server<H: ProtoHandler>(addr: SocketAddr, handler: H, n_workers: usize) {
     server::ProtoServer::start(addr, handler, n_workers);
@@ -20,4 +17,51 @@ pub trait ProtoHandler: Sized + Clone + Send + 'static {
 
     fn recv(&mut self, user: &mut User<Self>, message: Self::Proto);
     fn notify(&mut self, user: &mut User<Self>, message: Self::Message);
+}
+
+pub struct User<H: ProtoHandler> {
+    sender: Sender<H>,
+    broadcast: Option<H::Proto>,
+    echo: Option<H::Proto>,
+}
+
+impl<H: ProtoHandler> User<H> {
+    fn new(token: Token, sender: mio::Sender<WorkerMessage<H>>) -> Self {
+        User {
+            sender: Sender { token: token, sender: sender},
+            broadcast: None,
+            echo: None,
+        }
+    }
+
+    pub fn channel(&self) -> Sender<H> {
+        self.sender.clone()
+    }
+
+    pub fn broadcast(&mut self, message: H::Proto) {
+        self.broadcast = Some(message);
+    }
+
+    pub fn echo(&mut self, message: H::Proto) {
+        self.echo = Some(message);
+    }
+}
+
+#[derive(Clone)]
+pub struct Sender<H: ProtoHandler> {
+    token: Token,
+    sender: mio::Sender<WorkerMessage<H>>,
+}
+
+impl<H: ProtoHandler> Sender<H> {
+    pub fn send(&self, message: H::Message)
+                -> Result<(), mio::NotifyError<WorkerMessage<H>>> {
+        self.sender.send(WorkerMessage::HandlerMessage(self.token, message))
+    }
+}
+
+pub enum WorkerMessage<H: ProtoHandler> {
+    NewConnection(TcpStream),
+    HandlerMessage(Token, H::Message),
+    Broadcast(H::Proto),
 }
